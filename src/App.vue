115b98a7f9dc69d72881a1b2af80640bee7e85db
Stepan Alekseev <distephano7051511@gmail.com>
2025-06-27 14:20:04 +0300

    Copy project from gitlab

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="header__wrapper">
+        <RouterLink class="header__logo" to="/">Иконка Маруся</RouterLink>
+        <div class="header__main">
+          <RouterLink class="header__link" to="/">Главная</RouterLink>
+          <RouterLink class="header__link" to="/genres">Жанры</RouterLink>
+          <div class="custom-input">
+            <input class="custom-input__field" name="search" id="search" type="text" placeholder="Поиск">
+            <label for="search"></label>
+          </div>
+        </div>
+        <RouterLink to="/auth">Войти</RouterLink>
+      </div>
+    </div>
+  </header>
+  <RouterView />
+</template>
---
+++ src/router/router.ts
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory('/'),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/genres',
+      name: 'genres',
+      component: () => import('../views/GenresView.vue')
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: () => import('../views/AuthView.vue')
+    }
+  ]
+})
+
+export default router
